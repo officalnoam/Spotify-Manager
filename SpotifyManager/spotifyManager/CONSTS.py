0ec0a7ee7a67578d85efd48e483d9f9f3d937860
noamro <noamrooffical@gmail.com>
2024-04-02 09:16:31 -0700

    Create CONSTS file

---
+++ SpotifyManager/spotifyManager/CONSTS.py
@@ -0,0 +1,12 @@
+from json import load
+
+class SpotifySessionConsts:
+    with open("secrets.json", "r") as f:
+        secrets = load(f)
+    ID = secrets["client id"]
+    SECRET = secrets["client secret"]
+    URL = secrets["redirect url"]
+    SCOPE = secrets["scope"]
+
+    del(f)
+    del(secrets)
